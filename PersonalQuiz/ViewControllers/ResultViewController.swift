c3d119b6e3b49fc1f19bb94327669c4d56fe4e7a
gen <jigk@ma.com>
2023-08-14 02:11:59 +0300

    Algorithm improvement.

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -13,13 +13,24 @@ final class ResultViewController: UIViewController {
     
     var answers: [Answer]!
     
+    /* Это решение составлял очень долго и остался вопрос,
+     касательно корректного сноса строки. Я предположил снос по методам.
+     По сути получилась одна строка, приватный метод решил убрать.
+     */
     private var resultAnimal: Animal {
-        getResult()
+        answers
+            .map({$0.animal})
+            .reduce(into: [:]) {counts, animals in counts[animals, default: 0] += 1}
+            .sorted(by: { $0.value < $1.value })
+            .last! // Форс анврап допустим, без результата на экран не попасть
+            .key
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.navigationItem.hidesBackButton = true
+        
         bigResultLabel.text = "Вы - \(resultAnimal.rawValue)!"
         littleResultLabel.text = resultAnimal.definition
 
@@ -27,34 +38,5 @@ final class ResultViewController: UIViewController {
     
     @IBAction private func doneButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
-    }
-    
-    private func getResult() -> Animal {
-        let animals = answers.map {$0.animal}
-        let result = animals.reduce(into: [:]) { counts, animals in
-            counts[animals, default: 0] += 1
-        }
-        return result.sorted(by: { $0.value < $1.value } ).last!.key
-        
-        // Чтобы придти к решению выше, я потратил много времени. Но в случае, если оно кажется не читаемым, ниже оставлю своё первое решение.
-        
-        
-//        var resultScore: [Animal : Int] = [:]
-//        var result: Animal!
-//        for answer in answers {
-//            if resultScore[answer.animal] == nil {
-//                resultScore[answer.animal] = 1
-//            } else {
-//                resultScore[answer.animal]! += 1
-//            }
-//        }
-//
-//        for key in resultScore.keys {
-//            if resultScore[key] == resultScore.values.max() {
-//                result = key
-//            }
-//        }
-//
-//        return result
     }
 }
